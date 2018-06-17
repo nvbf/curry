3b033986b562f8d782552c6fe143d4b3325272c4
Sindre Øye Svendby <sinsvend@gmail.com>
2018-06-18 01:02:35 +0200

    Update future query

--- src/db/tournaments.js
+++ src/db/tournaments.js
@@ -45,26 +45,37 @@ AND sesong = 2017`;
 
 const tournamentQueryInTheFuture = () =>
   `Select 
-    TurneringsId as TournamentId,
-    Navn as name,
-    Finaledato as endDate,
-    Sesong as season,
-    Turneringstype as tournamentType,
-    TurneringsIdProfixio as tournamentIdProfixio,
-    KortnavnProfixio as shortNameProfixio,
-    StartDato as startDate,
-    Pameldingsfrist as deadline,
-    Turneringsleder as tournamentDirector,
-    TurneringEpost as mail,
-    TurneringTlf as phone,
-    Memo as description,
-    Spillested as playerVenue,
-    Betalingsinfo paymentInfo,
-    Arrangor as organizer,
-    Region as region 
-  FROM  Turnering
+  t.TurneringsId as TournamentId,
+  t.Navn as name,
+  t.Finaledato as endDate,
+  t.Sesong as season,
+  t.Turneringstype as tournamentType,
+  t.TurneringsIdProfixio as tournamentIdProfixio,
+  t.KortnavnProfixio as shortNameProfixio,
+  t.StartDato as startDate,
+  t.Pameldingsfrist as deadline,
+  t.Turneringsleder as tournamentDirector,
+  t.TurneringEpost as mail,
+  t.TurneringTlf as phone,
+  t.Memo as description,
+  t.Spillested as playerVenue,
+  Betalingsinfo paymentInfo,
+  t.Arrangor as organizer,
+  t.Region as region,
+  STUFF((
+    SELECT ',' + k.Klasse
+    FROM TurneringKlasse k
+    WHERE k.TurneringsId = t.TurneringsId
+    FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(100)'), 1, 1, '')
+  as classesAsText
+  FROM Turnering t
+  WHERE Turneringstype NOT LIKE 'WT%'
+  AND Turneringstype NOT LIKE 'CEV%'
+  AND Turneringstype NOT LIKE 'CEV%'
+  AND Turneringstype NOT LIKE 'FIVB%'
+  AND Turneringstype NOT LIKE 'NEVZA%'
   WHERE Finaledato > GETDATE()
-  ORDER BY StartDato ASC	
+  ORDER BY StartDato ASC
   `;
 
 const tournamentQuery = (id: number) =>
