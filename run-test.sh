94de60f12b941a12f39ddfde3ccb758c85dbb2fb
Sindre Øye Svendby <sinsvend@gmail.com>
2018-08-10 22:52:49 +0200

    Add correct test command

--- run-test.sh
+++ run-test.sh
@@ -4,8 +4,10 @@ PORT=3002
 PORT=$PORT NODE_ENV="test" babel-node src/index.js &
 
 ## Grab PID of background process
-NODE_PID=$!
-echo "Node.js app launching on PID: ${NODE_PID}. Port:$PORT Starting tests in 2 seconds."
+PID=$!
+PGID=$(ps axjf --sort=-pid | cut -d " " -f 2- | egrep ^$PID | head -1 | tr -s - " "  | cut -d " " -f 2)
+echo "PGID: $PGID"
+echo "Node.js app launching on PID: ${PID}. Port:$PORT Starting tests in 2 seconds."
 
 ## Give 2 sec for the node process to completely startup
 sleep 2
@@ -14,6 +16,7 @@ jest
 RETURN_VALUE=$?
 
 ## Kill background node process
-kill $NODE_PID
+echo "Killing app"
+pkill --pgroup $PGID
 
 exit $RETURN_VALUE
